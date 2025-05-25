9c24d5b7cd443bc825d9992a20cdfbdf3341c86f
aramossanchez <armandoramossanchez@gmail.com>
2025-05-25 17:54:51 +0200

    fix: arreglado error de traer params

--- app/blog/[id]/page.tsx
+++ app/blog/[id]/page.tsx
@@ -1,12 +1,12 @@
 import BlogArticleContainer from '@/containers/BlogArticleContainer/BlogArticleContainer';
 import { BLOG_ARTICLES } from '@/data/blog/Articles';
 
-export default function BlogArticlePage({
+export default async function BlogArticlePage({
   params,
 }: {
   params: { id: string };
 }) {
-  const { id } = params;
+  const { id } = await params;
   const article = BLOG_ARTICLES.find((article) => article.id === id) ?? null;
   return <BlogArticleContainer article={article} />;
 }
